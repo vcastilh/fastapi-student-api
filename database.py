5688524302bedd7f20bc6bfd5c1bc909af6b5214
vinex <notrealmailfornow@proton.me>
2024-08-27 16:01:43 -0300

    Change from MySql to Postgresqp

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from models import Base
 from dotenv import load_dotenv
@@ -13,7 +12,7 @@ MYSQL_PORT = os.getenv('MYSQL_PORT')
 MYSQL_DB = os.getenv('MYSQL_DB')
 
 # Define your MySQL connection URL
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
 # Create the engine for interacting with the database
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
